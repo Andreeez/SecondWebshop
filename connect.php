0f06dd2ff49b7c9ad8b64dcd5879ba1edaab7eba
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-04-05 14:50:40 +0200

    adds ini usage example

---
+++ connect.php
@@ -0,0 +1,5 @@
+<?php
+
+$dbsettings = parse_ini_file('./database.ini');
+
+$connection = mysqli_connect($dbsettings['address'], $dbsettings['username'], $dbsettings['password'], $dbsettings['dbname']);
