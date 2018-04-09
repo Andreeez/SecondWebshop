ce36d905329a45529c84d55112dda8229951c48e
amandaaab <amandaaab@users.noreply.github.com>
2018-04-09 16:05:29 +0200

    headerStruktur

--- connect.php
+++ connect.php
@@ -3,3 +3,16 @@
 $dbsettings = parse_ini_file('./database.ini');
 
 $connection = mysqli_connect($dbsettings['address'], $dbsettings['username'], $dbsettings['password'], $dbsettings['dbname']);
+
+$sql = "SELECT name FROM V5_SubCategory";
+$result = $connection->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> namn: ". $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
---
+++ header.php
@@ -0,0 +1,57 @@
+<?php define("webshopName", "The5Vise");?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo webshopName; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <!--<script src="main.js"></script>-->
+</head>
+<body>
+
+<div class="headerContainer">
+    <div class="headerRow">
+        <div class="headerItem" id="headerItem1">
+        <h2 class="webshopLogo"><?php echo webshopName; ?></h2>
+        </div>
+        <div class="headerItem" id="headerItem2">
+            <form class="searchForm">
+                <input type="text" id="searchInput" name="searchProduct" placeholder="S Ö K   P R O D U K T"/>
+            </form>
+        </div>
+        <div class="headerItem">
+            <div class="headerItembox">
+                <div class="headerItemRow" id="itemRow1">
+                    <img src="images/kontoSymbol.png" style="width:45px; height:40px; margin:auto"/>
+                </div>
+                <div class="headerItemRow">
+                    <form method="GET" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                        <button type="submit" name="headerLogin">Logga in /</input>
+                        <button type="submit" name="headerSignUp">Registera</input>
+                    </form>
+                </div>  
+            </div>
+        </div>
+
+        <div class="headerItem">
+            <img src="images/shoppingbag.png" id="shopBagLogo" style="width:45px; height:40px;"/>
+        </div>
+
+    </div>
+</div>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "GET"){
+        if(isset($_GET['headerLogin'])){
+            header('location: index.php');
+            }else{
+                die();
+            }
+        }
+    ?>
+
+    
+</body>
+</html>
+
