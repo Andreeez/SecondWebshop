965646aaf02b2975f83f81b3dbe263c9e9c96e9d
krasseman <32762094+krasseman@users.noreply.github.com>
2018-04-09 13:34:41 +0200

    Footer Newsletter

---
+++ footer.php
@@ -0,0 +1,36 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+
+</head>
+<body>
+    
+    <footer>
+  
+   <h4> Sign up for Newsletter </h4>
+    <form action="newsletter-signup.php" method="post"> 
+   
+    
+   <span>Name</span> <input type="text" name="name" id="name"/>
+    
+  <span>Email</span>  <input type="text" name="email" id="email"/>
+    <input type="submit" name=submit value="Submit"/> 
+    </form> 
+  
+
+    </footer>
+
+
+</body>
+</html>
