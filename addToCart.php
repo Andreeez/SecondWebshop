2bf8afd2f53ddd0141ceace0fdcc6e2fb4b811ab
krasseman <32762094+krasseman@users.noreply.github.com>
2018-04-10 10:33:54 +0200

    ..

--- addToCart.php
+++ addToCart.php
@@ -9,10 +9,12 @@ $id=$_POST['id'];
 
 if(!isset($_SESSION['cart'])){
     $_SESSION['cart'] = array();
+    array_push($_SESSION['cart'], $id);  
+    
+   
 }
 
-array_push($_SESSION['cart'], $id);  
-
-
 echo json_encode($_SESSION['cart']);
+
+}
 ?>
