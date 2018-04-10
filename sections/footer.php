96aabc1043017f3ae30d8e1044fd3feb0b606499
krasseman <32762094+krasseman@users.noreply.github.com>
2018-04-10 13:59:38 +0200

    Footer

--- sections/footer.php
+++ sections/footer.php
@@ -1,33 +1,33 @@
-<?php
-
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-
-</head>
-<body>
-    
     <footer>
-  
-   <h4> Sign up for Newsletter </h4>
-    <form action="newsletter-signup.php" method="post"> 
-   
-    
-   <span>Name</span> <input type="text" name="name" id="name"/>
-    
-  <span>Email</span>  <input type="text" name="email" id="email"/>
-    <input type="submit" name=submit value="Submit"/> 
+    <?php
+
+    $subscribed =   "<div id ='alreadySubscribed'>
+    Tack för att du anmält dig för nyhetsbrev! 
+    </div>";
+
+    $newsletterBox =    "<div id='newsletterBox'> 
+    <h4>Anmäl dig för Nyhetsbrev här </h4>
+    <form action='newsletter-signup.php' method='post'> 
+
+    <span>Name</span> <input type='text' name='name' id='name'/>
+
+    <span>Email</span>  <input type='text' name='email' id='email'/>
+    <input type='submit' name=submit value='Submit'/> 
     </form> 
+
+    </div>";
+
+if(isset($_COOKIE["newsletter"])){
+    echo 
+   $_COOKIE['newsletter']."!".$subscribed;
   
+} else{
+    
+   echo
+     $newsletterBox;
+
+}
+?>
 
     </footer>
 
