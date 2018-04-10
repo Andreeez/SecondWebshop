aa04ca40da2e7163075bc7fdc3935f231bd9e155
antonpetersson <antonpetersson@users.noreply.github.com>
2018-04-10 13:26:02 +0200

    skapat shoppingcart och flyttat bilder.

--- product.php
+++ product.php
@@ -1,35 +1,11 @@
 <?php
     session_start();    
     require './sections/header.php';
-    require './connect/connect.php';
-    
+    require './classes/productClasses.php';
+     
 
-     class Product{
-        public $id;
-        function __construct($id){
-             $this->id = $id;
-        }
-
-        function printProduct(){
-            global $connection;
-            $sql = "SELECT * FROM v5_products where id = " . $this->id;
-            $result = $connection->query($sql);
-            $row = $result->fetch_assoc();
-            echo $row['title'] . " (" . $row['year'] .")</br>";
-            echo "<img src='./images/" . $row['id'] . ".jpg' alt='" . $row["title"] . "'></br>";
-            echo "<button onclick='addToCartButton(" . $row['id'] . ")'>Lägg till i kundvagn</button>";
-            echo $row['price'] . " kr </br>" . $row['description'];
-            
-        }
-
-
-      }
-
-
-  
-
-    $myProduct = new Product(5);
-    $myProduct->printProduct();
+    $productPage = new Product(5);
+    $productPage->printProductPage();
 
     
 ?>
