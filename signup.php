324d5b3c9d580583668148eaae810ca362d18e01
amandaaab <amandaaab@users.noreply.github.com>
2018-04-11 12:13:21 +0200

    signup början

---
+++ signup.php
@@ -0,0 +1,26 @@
+<?php
+require 'sections/header.php';
+require 'connect/connect.php';
+?>
+
+<div class="loginContainer">
+    <div class="loginBox">
+        <div class="loginTextContainer">
+            <h2 class="loginText">Registrera dig</h2>
+            <p class="loginNotUser">Har du redan konto, vänligen logga in<a href="#">här</a></p>
+        </div>
+        <form method="POST" class="signIn">
+            <input type="text" class="inputSignUp" placeholder="Förnamn" name="fName"/>
+            <input type="text" class="inputSignUp" placeholder="Efternamn" name="eName"/>
+            <input type="text" class="inputSignUp" placeholder="Email" name="email" id="inputSignUpUser"/>
+            <input type="text" class="inputSignUp" placeholder="Adress" name="adress"/>
+            <input type="text" class="inputSignUp" placeholder="Postnummer" name="postCode"/>
+            <input type="text" class="inputSignUp" placeholder="Stad" name="city"/>
+            <input type="text" class="inputSignUp" placeholder="Telefonnummer" name="phone"/>
+            
+            <input type="password" class="inputSignUp" placeholder="Password" name="newPass"/>
+            <input type="password" class="inputSignUp" placeholder="Bekräfta Lösenord" name="confirmPass"/>
+            <input type="submit" name="signUpNewUser"/>
+        </form>
+    </div>
+</div>
