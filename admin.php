25df90feb91b45937295864d35a370c04741f38f
Andreeez <32762098+Andreeez@users.noreply.github.com>
2018-04-13 13:08:42 +0200

    Skicka funktion funkar nu

--- admin.php
+++ admin.php
@@ -55,7 +55,7 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
 
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    //$adminKey = $POST['adminKey'];
+    $adminKey = $_POST['adminKey'];
     $newAdmin2 = new adminUpdateSendDate();
     $newAdmin2->printSavedOrders();
 
