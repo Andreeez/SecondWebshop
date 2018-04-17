ba21ea61b07cf11bbe026e52722f870d0b758370
amandaaab <amandaaab@users.noreply.github.com>
2018-04-17 12:59:00 +0200

    lite styling

--- login.php
+++ login.php
@@ -12,8 +12,8 @@ require './login-function.php';
         </div>
         <form method="POST" class="loginForm" action="./login.php">
             <input type="text" class="inputLogin" placeholder="Email" name="userName" id="inputLoginUser"/>
-            <input type="password" class="inputLogin" placeholder="Lösenord" name="userPass"/>
-            <input type="submit" name="submitLogin"/>
+            <input type="password" class="inputLogin" placeholder="Lösenord" name="userPass" id="userPass"/>
+            <input type="submit" id="loginButton" name="submitLogin" value="Logga in"/>
         </form>
     </div>
 </div>
